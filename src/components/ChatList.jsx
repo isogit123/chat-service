6908e0acc8892eb324b232763970e366220436e1
Islam Mohamed Ahmed Mohamed Abou Tabl <tot13_p@hotmail.com>
2019-11-25 17:01:21 +0200

    c

---
+++ src/components/ChatList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ChatList extends Component {
+    state = { 
+        peers = []
+     }
+    render() { 
+        return ( 
+            
+         );
+    }
+}
+ 
+export default ChatList;
--- src/dal.js
+++ src/dal.js
@@ -1,6 +1,6 @@
 
-  export const addChat = (peerName) => {
-    var dbPromise = idb.open('msgs', 1, function(upgradeDb) {
+  export const getDbPromise = () => {
+    return idb.open('msgs', 1, function(upgradeDb) {
         switch (upgradeDb.oldVersion) {
           case 0:
             var chatsStore = upgradeDb.createObjectStore('chats', {autoIncrement: true});
@@ -8,4 +8,34 @@
             chatsStore.createIndex('peer', 'peer', {unique: false});
         }
       });
+  }
+
+  export const addMsg = (msg) => {
+    getDbPromise().then(function (db){
+      var tx = db.transaction('chats', 'readwrite');
+      var store = tx.objectStore('chats');
+      store.add(msg);
+      return tx.complete;
+    }).then(function() {
+      console.log('added item');
+    });
+    
+  }
+
+  export const getChat = (peer) => {
+    getDbPromise().then(function (db){
+      var tx = db.transaction('chats', 'readwrite');
+      var store = tx.objectStore('chats');
+      return store.openCursor();
+    }).then(function logItems(cursor) {
+      if (!cursor) {
+        return;
+      }
+      console.log('Cursored at:', cursor.key);
+      for (var field in cursor.value) {
+        console.log(cursor.value[field]);
+      }
+      return cursor.continue().then(logItems);
+    });
+    
   }
